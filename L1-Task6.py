0f2977263dd50613c2bffd0f5d92e0ba03b3a741
asenyuta <var99@list.ru>
2020-05-16 17:24:42 +0300

    Создаем новую ветку Lesson-2

--- L1-Task6.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Количество км в 1-й день: "))
-b = float(input("Хотим пробежать км: "))
-
-day = 1
-while a < b:
-    a = a * 1.1
-    day = day + 1
-
-print(f"Дистанция покрыта за {day} дней")
